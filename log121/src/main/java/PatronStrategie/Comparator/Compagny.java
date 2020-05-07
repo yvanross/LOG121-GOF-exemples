3a0b924eb023115d2cd8198cb927fd4ffa2915ff
Yvan Ross <Yvan.ross@gmail.com>
2020-05-07 17:50:09 -0400

    test pull request

--- log121/src/main/java/PatronStrategie/Comparator/Compagny.java
+++ log121/src/main/java/PatronStrategie/Comparator/Compagny.java
@@ -3,7 +3,6 @@ package PatronStrategie.Comparator;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-//import java.util.Iterator;
 import java.util.List;
 
 public class Compagny {
