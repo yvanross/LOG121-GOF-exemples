3a6547f8d874689c40768c6ed16ad1403c65ad90
doremyk <kevin.doremy-laferriere.1@ens.etsmtl.ca>
2020-07-31 18:22:55 -0400

    Fix warning and Change file name Graphed for Graph

--- src/test/java/Prototype/ConcretePrototype.java
+++ src/test/java/Prototype/ConcretePrototype.java
@@ -2,78 +2,28 @@ package Prototype;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import Proptoptype.ConcretePrototype.ConcretePrototype1;
 import Proptoptype.ConcretePrototype.ConcretePrototype2;
 
-public class ConcretePrototype  {
+public class ConcretePrototype {
 
-  @Test
-  public void createClone() throws CloneNotSupportedException {
-    final ConcretePrototype1 concretePrototype1 =  new ConcretePrototype1();
-   final ConcretePrototype1 concretePrototype1Cloned = (ConcretePrototype1) concretePrototype1.clone();
+    @Test
+    public void createClone() throws CloneNotSupportedException {
+        final ConcretePrototype1 concretePrototype1 = new ConcretePrototype1();
+        final ConcretePrototype1 concretePrototype1Cloned = (ConcretePrototype1) concretePrototype1.clone();
 
-   assertNotEquals(concretePrototype1,concretePrototype1Cloned);
-   assertEquals(concretePrototype1.getType(), concretePrototype1Cloned.getType());
-   
-   final ConcretePrototype2 concretePrototype2 = new ConcretePrototype2();
-   final ConcretePrototype2 concretePrototype2Cloned = (ConcretePrototype2) concretePrototype2.clone();
+        assertNotEquals(concretePrototype1, concretePrototype1Cloned);
+        assertEquals(concretePrototype1.getType(), concretePrototype1Cloned.getType());
 
-   assertNotEquals(concretePrototype2,concretePrototype2Cloned);   
-  }
+        final ConcretePrototype2 concretePrototype2 = new ConcretePrototype2();
+        final ConcretePrototype2 concretePrototype2Cloned = (ConcretePrototype2) concretePrototype2.clone();
+
+        assertNotEquals(concretePrototype2, concretePrototype2Cloned);
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Object obj = new Object();
 // assertTrue(concretePrototype1 instanceof Object);
